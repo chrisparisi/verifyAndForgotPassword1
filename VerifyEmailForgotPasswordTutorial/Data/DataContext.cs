15a8d310e890ee0b7db32e1282462a252f7c36a9
Chris Parisi <the.chris.parisi@gmail.com>
2022-11-07 10:27:24 -0800

    project done

---
+++ VerifyEmailForgotPasswordTutorial/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace VerifyEmailForgotPasswordTutorial.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
+        {
+            base.OnConfiguring(optionsbuilder);
+            optionsbuilder.UseSqlServer("server=ATLAS;database=userdb;trusted_connection=true;");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
